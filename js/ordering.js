41dacecca341200d3254b6f73eb46040c6224b12
TH <757901093@qq.com>
2018-03-17 12:55:22 +0800

    add customer

--- js/ordering.js
+++ js/ordering.js
@@ -14,15 +14,22 @@
 		return $(window).width() -21
 	},height:function(){
 		return $(window).height()
-	}}).find(".order-box").css({top:function(){
-		return (orderbgc.height()-orderbgc.find(".order-box").height())/2
-	},left:function(){
+	}}).find(".order-box").css({"top":function(){
+		return (orderbgc.height()-400)/2
+	},"left":function(){
 		return (orderbgc.width()-orderbgc.find(".order-box").width())/2
 	}})
 	$(window).scroll(function(){
 		orderbgc.css("top",$(this).scrollTop())
 	})
+	//图片点击添加弹窗
 	imglist.click(function(){
 		orderbgc.css("display","block")
 	})
+	
+	//关闭弹窗
+	orderbgc.find(".close").click(function(){
+		orderbgc.css("display","none")
+	})
+	
 })
